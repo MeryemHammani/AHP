1aa768b813b4d2af3f3e2080bbb5a7c5ca65b447
MeryemHammani <meryem.hammani@usmba.ac.ma>
2023-05-27 14:40:58 +0200

    upload files

---
+++ main.py
@@ -0,0 +1,18 @@
+from AHP import*
+import numpy as np
+A = np.array([[1, 1, 7, 4], [1, 1, 3, 5], [1/7, 1/3, 1, 3], [1/4, 1/5, 1/3, 1] ])
+#vectPriorite(A)
+#print(Matrix(4))
+AHP()
+# m=np.ones((4,3))
+v = np.array([[1, 2, 1], [1, 1, 1]])
+# # print(v.transpose())
+# u = np.array([3, 2])
+# print(u*2)
+# print((v.T)*u)
+# print(np.concatenate((v,u)))
+# # print((m*v))
+# a=1/7
+#print(a)
+
+
